dce27d15e5ea1143484fee8d99bc1bbab54630b7
Flo-IT <flodino03@gmail.com>
2020-12-19 23:38:34 +0100

    Fix: Change location of token (body -> header)

--- Server/middlewares/verifyTokenMiddleware.js
+++ Server/middlewares/verifyTokenMiddleware.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken")
 
 const verifyTokenMiddleware = (req, res, next) => {
-    const token = req.body.token
+    // The token s stored in the header
+    const token = req.headers["authorization"]
 
     if (!token) return res.status(401).json({ error: "Access denied" })
 
