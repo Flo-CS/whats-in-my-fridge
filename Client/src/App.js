6c0fd44481d660d5c93a83cad7080c888575d4d7
Flo-IT <flodino03@gmail.com>
2020-12-20 18:17:03 +0100

    Refactor: Some useless things cleaning

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,14 +4,7 @@ import { hot } from 'react-hot-loader/root';
 
 class App extends React.Component {
   render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1 className="text-4xl text-white bg-black">
-          Hello {name}
-        </h1>
-      </>
-    );
+    return <div></div>
   }
 }
 
