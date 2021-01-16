2fe44775116356c12818105d9b87787082b43e7f
Flo-IT <flodino03@gmail.com>
2021-01-16 11:46:30 +0100

    Feat: Improve product card style

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -27,22 +27,26 @@ export default function ProductCard({ product }) {
         product.barcode
       );
 
-      setProductQuantity((quantity) => newQuantity);
+      setProductQuantity(newQuantity);
     } catch (error) {}
   }
 
   return (
     <div className="product-card">
-      <div className="product-card__header">
-        <img
-          className="product-card__image"
-          alt="Product"
-          src={product.data?.image_url}
-        ></img>
-        <p className="product-card__name">
-          {product.data?.product_name} - {product.data?.brands} -{" "}
-          {product.barcode}
-        </p>
+      <div className="product-card__content">
+        <div className="product-card__header">
+          <p className="product-card__name">
+            {product.data?.product_name} - {product.data?.brands} -{" "}
+            {product.barcode}
+          </p>
+        </div>
+        <div className="product-card__body">
+          <img
+            className="product-card__image"
+            alt="Product"
+            src={product.data?.image_url}
+          ></img>
+        </div>
       </div>
 
       <div className="product-card__controls">
