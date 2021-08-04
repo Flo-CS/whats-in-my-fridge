96e98b8ea84e4a512d8351e78d6234e3252aebfa
Flo-IT <flodino03@gmail.com>
2021-08-04 09:31:58 +0200

    Refactor: minor refactoring

--- client/src/components/input/SearchInput.jsx
+++ client/src/components/input/SearchInput.jsx
@@ -4,9 +4,9 @@ import {ReactComponent as SearchIcon} from "./../../assets/icons/search.svg";
 import "./SearchInput.scss";
 
 export default function SearchInput({...rest}) {
-    return <div className="search-input__wrapper">
+    return <div className="search-input">
         <SearchIcon className="search-input__search-icon"/>
-        <input className="search-input" {...rest}/>
+        <input className="search-input__input" {...rest}/>
     </div>;
 }
 
